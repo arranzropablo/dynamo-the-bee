22817284704ba177e56d29f89bb37f1cd621c7b7
Pablo <Pablo.ArranzRopero@zooplus.com>
2019-04-15 12:36:12 +0200

    trying to get items from table

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var tpl *template.Template
@@ -15,23 +16,33 @@ func init() {
 }
 
 func main() {
-	result, err := db.ListTables().All()
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println(result)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := tpl.ExecuteTemplate(w, "index.gohtml", struct{Tables []string}{result})
+		result, err := db.ListTables().All()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println(result)
+		err = tpl.ExecuteTemplate(w, "index.gohtml", struct{Tables []string}{result})
 		if err != nil {
 			log.Fatalln(err)
 		}
 	})
-	err = http.ListenAndServe(":8080", nil)
+
+	//manejador del nombre para enseñar los elementos
+	http.HandleFunc("/table/", func(w http.ResponseWriter, r *http.Request) {
+		table := db.Table(strings.TrimPrefix(r.URL.Path, "/table/"))
+		//fmt.Println(table.Describe().Run())
+
+		table.Put(struct{b_id int}{2}).Run()
+		q := table.Get("b_id", 2)
+		fmt.Println(q)
+	})
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
